a31250814b916ac73a5f0f6dc176e9fd0ed1668d
CalebAdelaja <calebadelaja0@gmail.com>
2026-08-30 03:09:40 +0100

    August 30

--- dummyApi.js
+++ dummyApi.js
@@ -1,12 +1,12 @@
-const productsGrid = document.getElementById('productsGrid')
-const navBar = document.getElementById('navBar');
-const footer = document.getElementById('footer')
-const cartCount = document.getElementById('cartCount')
-const loader = document.getElementById('loader')
-const loadingText = document.getElementById('loadingText')
-
-const modeControl = document.querySelector('.mode-control');
 const Body = document.body;
+const productsGrid = document.getElementById('productsGrid');
+const navBar = document.getElementById('navBar');
+const footer = document.getElementById('footer');
+const cartCount = document.getElementById('cartCount');
+const loader = document.getElementById('loader');
+const loadingText = document.getElementById('loadingText');
+const modeControl = document.querySelector('.mode-control');
+
 if (localStorage.getItem('darkMode') === 'dark') {
     Body.classList.add('dark');
 }
@@ -15,7 +15,7 @@ modeControl.addEventListener('click', () => {
     Body.classList.toggle('dark');       
             
     if (Body.classList.contains('dark')) {
-            localStorage.setItem('darkMode', 'dark');
+        localStorage.setItem('darkMode', 'dark');
     } else {
         localStorage.setItem('darkMode', 'white');
     }
@@ -24,8 +24,8 @@ modeControl.addEventListener('click', () => {
 const cart = JSON.parse(localStorage.getItem("cart")) || [];      
 updateCartCount()
 // cartCount.textContent = cart.length;
-let products = [];//This is a global scope variable 
-console.log(products)
+let products = []; //This is a global scope variable 
+// console.log(products)
 async function fetchProducts() {
     try {
         loader.style.display = 'flex'
@@ -41,13 +41,13 @@ async function fetchProducts() {
             footer.style.display = 'block'
         }
         let productData = await response.json();
-        console.log(productData)
+        // console.log(productData)
         products = productData.products;
-        console.log(typeof products)
-        console.log(products)
-        console.log(productData.products)
-        productData.products.forEach((product) => {
-            console.log(product)
+        // console.log(typeof products)
+        // console.log(products)
+        // console.log(productData.products)
+        products.forEach((product) => {
+            // console.log(product)
             const productsCard = document.createElement('div')
             productsCard.classList.add("product-card") 
             const safeDescription = product.description.replace(/"/g, '&quot;').replace(/\n/g, ' ')
@@ -70,27 +70,27 @@ async function fetchProducts() {
 
                 const addToCartBtn = productsCard.querySelector(".add-to-cart");
                 addToCartBtn.addEventListener("click", (event) => {
-                    console.log(event.target);
+                    // console.log(event.target);
                     const productId = Number(event.target.dataset.id);
-                    console.log(productId)
+                    // console.log(productId)
                     const selectedProduct = products.find((item) => {
-                        console.log(item)
-                        console.log(item.id)
+                        // console.log(item)
+                        // console.log(item.id)
                         return item.id === productId;
                     });
                     // selectedProduct.quantity = 1;
-                    console.log(selectedProduct)
+                    // console.log(selectedProduct)
                     const newProduct = {
                         ...selectedProduct,
                         quantity: 1
                     };
-                    console.log(newProduct);
+                    // console.log(newProduct);
                     const productExist = cart.find((item) => {
-                        console.log(item)
-                        console.log(item.id)
+                        // console.log(item)
+                        // console.log(item.id)
                         return item.id === productId;
                     });
-                    console.log(productExist)
+                    // console.log(productExist)
                     if(productExist) {
                         productExist.quantity++;
                     }else {
@@ -102,7 +102,7 @@ async function fetchProducts() {
                     localStorage.setItem("cart", JSON.stringify(cart));
                     updateCartCount()
                     // cartCount.textContent = cart.length;
-                    console.log(cart);
+                    // console.log(cart);
                 });
 
                 productsGrid.appendChild(productsCard)
@@ -118,16 +118,22 @@ async function fetchProducts() {
         navBar.style.display = 'none'
         footer.style.display = 'none'
         let response = await fetch('404.html')
+        console.log(response)
         let errorPage = await response.text()
         console.log(errorPage)
         productsGrid.innerHTML = errorPage;
+        console.log(productsGrid)
+        const reloadBtn = document.getElementById("reloadBtn");
+        reloadBtn.addEventListener("click", () => {
+            location.reload();
+        });
     }
 
 }
+
 function updateCartCount() {
     // cartCount.textContent = cart.length;
     cartCount.textContent = cart.reduce((acc, curr) => acc + curr.quantity, 0); 
 }
 
 fetchProducts()
-
